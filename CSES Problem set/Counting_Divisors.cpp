4de0422665b93a01cc69b018e89ade44477d25f4
Ashif <67032510+AsifCR@users.noreply.github.com>
2021-09-12 13:06:49 +0600

    new

---
+++ CSES Problem set/Counting_Divisors.cpp
@@ -0,0 +1,62 @@
+/**   In the name of Allah (SWT).
+*
+*    author:  Asif
+*
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef    long long                       ll;
+typedef    unsigned long long              ull;
+typedef    vector<int>                     vi;
+typedef    vector<vi>                      vvi;
+typedef    vector<ll>                      vl;
+typedef    vector<vl>                      vvl;
+typedef    vector<char>                    vc;
+typedef    vector<vc>                      vvc;
+typedef    pair<int, int>                  pii;
+typedef    pair<ll, ll>                    pll;
+typedef    pair<string, string>            pss;
+typedef    map<int, int>                   mii;
+typedef    unordered_map<int, int>         umap_ii;
+typedef    unordered_map<string, int>      umap_si;
+
+#define    Fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+#define    INF                             (int)1e9
+#define    MOD                             1000000007
+#define    pb                              push_back
+#define    eb                              emplace_back
+#define    imax                            INT_MAX
+#define    imin                            INT_MIN
+#define    PI                              3.1415926535897932384626433832795
+#define    VSORT                           sort(arr.begin(), arr.end())
+#define    VRSORT                          sort(arr.rbegin(), arr.rend())
+#define    V1SORT                          sort(v1.begin(), v1.end())
+#define    V1RSORT                         sort(v1.rbegin(), v1.rend())
+
+void busted(){
+    int x; cin >> x;
+    int divisors = 0;
+    for(int i = 1; i * i <= x; i++){
+        if(x % i == 0){
+            if(x / i == i){
+                divisors++;
+            }
+            else{
+                divisors += 2;
+            }
+        }
+    }
+    cout << divisors << '\n';
+    return;
+}
+
+int32_t main(){
+    Fast
+    int tc; cin >> tc;
+    while(tc--)
+        busted();
+    return 0;
+}
