e62c151676d127a0465d2df7026452bf25a18d58
ashif29 <asifcr75@gmail.com>
2024-01-02 21:52:19 -0800

    Added Pbds.cpp in DSA

---
+++ DSA & Others/pbds.cpp
@@ -0,0 +1,33 @@
+/**
+ *    author:  _ASIF
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// Required snippits
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template <class T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void busted(){
+    // Operations
+
+
+    // oset <int> st;
+    // st.insert();
+    // st.order_of_key();
+}
+
+int32_t main(){
+    Fast
+    int tests; cin >> tests;
+    while(tests--){
+        busted();
+    }
+    return 0;
+}
